8a77df0d9958a54e9951d5aeda255fac54741fec
Stephens6623 <stephenspargo134@gmail.com>
2025-07-09 19:32:08 -0400

    made server

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+
+// create an Express application
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/api/health", (req,res) => {
+  res.status(200).json({ message: "API is healthy" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
